21617a79b12912e1a71aa4711fb9a1d7d42206d9
MeenaSatyavarapu <119624262+MeenaSatyavarapu@users.noreply.github.com>
2023-03-30 17:16:04 -0400

    added products from github dev

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,4 +1,9 @@
 const Products = () =>{
-    return <div> Products Page</div>
+    return <>
+         <div> Products Page</div>
+         <h3> Mobile </h3>
+         <h3> Appliances </h3>
+         <h3> clothing </h3>
+    </>
 }
 export default Products
