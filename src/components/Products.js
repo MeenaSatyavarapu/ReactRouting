23d14eda02cd4c152ab270b7483dfcf586e15872
MeenaSatyavarapu <divyasree.meenu@gmail.com>
2023-03-30 17:10:08 -0400

    Product branch

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,4 +1,11 @@
 const Products = () =>{
-    return <div> Products Page</div>
+    return <>
+    <div> Products Page</div>
+    <div>
+        <h3> Appliances </h3>
+        <h3> Mobile </h3>
+        <h3> Clothing </h3>
+    </div>
+    </>
 }
 export default Products
